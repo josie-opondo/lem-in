e253aa26403bd37c2884c4479828181b2c3aa0cb
josie-opondo <josephineopondo5@gmail.com>
2024-12-09 20:45:26 +0300

    fix: update the Links and Ants structure

--- structure/structure.go
+++ structure/structure.go
@@ -1,14 +1,24 @@
 package structure
 
 type Room struct {
-	Name string
-	X, Y int
-	IsStart bool
-	IsEnd bool
+	Name     string
+	X, Y     int
+	IsStart  bool
+	IsEnd    bool
 	Occupied bool
 }
 
 type Graph struct {
 	Rooms map[string]*Room
-	Links map[string][]string
+	Links []Link
+}
+
+type Ant struct {
+	ID       int
+	Position string
+}
+
+type Link struct {
+	Room1 string
+	Room2 string
 }
