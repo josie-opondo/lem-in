b7288fd9d4e2fabb1cafd3c018756c08fe77c638
josie-opondo <josephineopondo5@gmail.com>
2024-12-09 20:48:19 +0300

    chore: call the parser function to test it's output

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"lem-in/utils"
+)
+
 func main() {
-	
+	graph, ants, err := utils.Parser()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Parsed Graph: %+v\n", graph)
+	fmt.Printf("Number of Ants: %d\n", ants)
 }
