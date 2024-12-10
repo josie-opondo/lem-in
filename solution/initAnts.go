1f2b9bd2c402d9f7cd0b151e7a36e1abca0a51f2
josie-opondo <josephineopondo5@gmail.com>
2024-12-10 09:18:37 +0300

    attempt to position ants

--- solution/initAnts.go
+++ solution/initAnts.go
@@ -1,1 +1,12 @@
 package solution
+
+import "lem-in/structure"
+
+func InitializeAnts(antCount int, startRoom *structure.Room) map[int]string {
+	antPositions := make(map[int]string)
+	for i := 1; i <= antCount; i++ {
+		antPositions[i] = startRoom.Name
+	}
+	startRoom.Occupied = antCount
+	return antPositions
+}
